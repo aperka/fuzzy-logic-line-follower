c3098691b49287d04f9e3ca1536673c3ee7967cf
Arkadiusz Perka <arkadiusz@MacBook-Pro-Arkadiusz.local>
2017-01-18 10:58:27 +0100

    ADD: MAP COMMENTS

--- map.py
+++ map.py
@@ -1,8 +1,17 @@
+""" Module: 
+    Show GUI window with map and robot image.
+    Read and store map (pixel values)
+    """
+
 import pygame
 from PIL import Image
 
 class Map(object):
     def __init__(self, bgImPath, rbImPath):
+        """ Set display size, name, clock and loads map, robot positon, array of map pixel values 
+            @param bgImPath - Path to map image 
+            @param rbImPath - Path to robot image
+            """
         pygame.init()
         self.gameDisplay = pygame.display.set_mode((800,600))
         pygame.display.set_caption('Line Follower')
@@ -14,7 +23,13 @@ class Map(object):
 
 
     def set_RobotPos(self, x, y, angle):
-        """rotate an image while keeping its center and size"""
+        """ Set robot image on selected position and rotate it 
+            @param x - x coordinate of robot's center
+            @param y - y coordinate of robot's center
+            @param x - x coordinate of robot's center
+            @param angle - robot's angle
+            
+            """
         orig_rect = self.carImg.get_rect()
         rot_image = pygame.transform.rotate(self.carImg, angle)
         rot_rect = orig_rect.copy()
@@ -24,12 +39,18 @@ class Map(object):
         self.gameDisplay.blit(rot_image, (x-50,y-50))
 
     def map(self, x,y):
+        """ Sets map image on selcted positon 
+            @param x - x coordinate of left map corner
+            @param y - y coordinate of left map corner
+            """
         self.gameDisplay.blit(self.bgImg, (x,y))
 
     def print_(self):
+        """ Update GUI"""
         pygame.display.update()
 
     def check_event(self):
+        """ Check if user want to close app """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -37,4 +58,8 @@ class Map(object):
         return "work"
 
     def get_PixVal(self, x, y):
+        """ Return selected map's pixel value
+            @param x - x coordinate of a pixel
+            @param y - y coordinate of a pixel
+            """
         return self.pixels[x,y]
